b2aaf88b8b6e9f82c073f6ad468ba14ec8482d14
Humphrey Ikhalea <84229187+humphreydev6@users.noreply.github.com>
2023-07-22 15:31:57 +0100

    Printing Strings

--- printf/print_age.c
+++ printf/print_age.c
@@ -7,8 +7,17 @@ int main(void){
 
   //adding more than one format speciation in "printf"
   int x = 5, y = 10;
-  printf ( "x is %-8d and y is %2d\n", x, y );
+  printf ( "x is %02d and y is %03d\n", x, y );
+
+  int year = 15;
+  char * grade;
+  if ( year == 13 ) grade = "junior";
+
+  else grade = "Senior";
+
+  printf ( "%s is a %s\n", "Fred", grade );
 
   return (0);
+
 
 }
