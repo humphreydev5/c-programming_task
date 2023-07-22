0bc206383c044ff87062b70361a4efa1610383e7
Humphrey Ikhalea <84229187+humphreydev6@users.noreply.github.com>
2023-07-22 13:48:15 +0100

    adding more than one format speciation in printf

---
+++ printf/print_age.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void){
+  int age;
+  age = 30;
+  printf ( "I am %d years old\n", age );
+
+  //adding more than one format speciation in "printf"
+  int x = 5, y = 10;
+  printf ( "x is %d and y is %d\n", x, y );
+
+  return (0);
+
+}
