96a35bde8010b9911320dee0fcfe82946106cf6d
Humphrey Ikhalea <84229187+humphreydev6@users.noreply.github.com>
2023-08-10 12:34:42 +0100

    Learning process ID

---
+++ ss_program/test1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(void)
+{
+  int number = 17;
+
+  pid_t process_id;
+  process_id = getppid();
+
+  printf("%d\n", number);
+  printf("Print the process ID of the program: %u", process_id);
+
+  return(0);
+
+}
