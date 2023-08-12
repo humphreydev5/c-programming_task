ea15a0398939fd44c439764f49ce6e8be5eddf31
Humphrey Ikhalea <84229187+humphreydev6@users.noreply.github.com>
2023-08-12 15:57:30 +0100

    sum of add

--- ss_program/fun_sum.c
+++ ss_program/fun_sum.c
@@ -1,16 +1,27 @@
 #include <stdio.h>
 
-int sumAdd(int A, int B);
+int sum(int i, int j);
+int sum1(int a, int b);
 
 int main(void)
 {
-  int n = sumAdd(45, 56);
-  printf("The result is: %d\n", n);
+  int n, k;
+  n = sum(45, 12);
+  k = sum1(5, 82);
+  printf(" add = %d\n sub = %d\n", n, k);
   return(0);
 }
 
-int sumAdd(int A, int B)
+int sum(int i, int j)
 {
-  int result = A + B;
-  return(result);
+  int results;
+  results = i + j;
+  return(results);
+}
+
+int sum1(int a, int b)
+{
+  int results1;
+  results1 = a - b;
+  return(results1);
 }
