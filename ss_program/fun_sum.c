d70cc494072395d9e1c5c221b652ed576e91b427
Humphrey Ikhalea <84229187+humphreydev6@users.noreply.github.com>
2023-08-12 16:11:33 +0100

    adding the process id

--- ss_program/fun_sum.c
+++ ss_program/fun_sum.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
+#include <unistd.h>
 
 int sum(int i, int j);
 int sum1(int a, int b);
 
 int main(void)
 {
+  pid_t pid;
+  pid = getpid();
   int n, k;
   n = sum(45, 12);
   k = sum1(5, 82);
-  printf(" add = %d\n sub = %d\n", n, k);
+  printf(" add = %d\t sub = %d\n", n, k);
+  printf("Print the process %u\n", pid );
   return(0);
 }
 
